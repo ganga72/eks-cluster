d8faafbc22bee36e2d2ee0b92b29601f1061a6b7
Ganga <gangadharamv08@gmail.com>
2026-02-27 18:31:30 +0530

    output results

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "eks_cluster_name" {
+  value = aws_eks_cluster.eks.name
+}
+
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.eks.endpoint
+}
+
+output "eks_cluster_arn" {
+  value = aws_eks_cluster.eks.arn
+}
+
+output "eks_node_group_name" {
+  value = aws_eks_node_group.managed_nodes.node_group_name
+}
